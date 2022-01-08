87bed077fd8c364ae6c951fadb56eaf2344a3f97
szarbartosz <szarbartosz@gmail.com>
2022-01-08 19:20:04 +0100

    implement configurable sensors and sprinklers

--- main.py
+++ main.py
@@ -5,16 +5,19 @@ import ssl
 import certifi
 import geopy.geocoders
 
-def prepare_sprinklers(sprinklersArr, sectorsNo):
-    sprinklerId = 1
 
+def prepare_sensors(sensorsArr, sectorsNo):
+    for i in range(sectorsNo):
+        sensorId = input(f"Podaj id sensora w sektorze {i + 1}: ")
+        sensorsArr.append(sensorId)
+
+def prepare_sprinklers(sprinklersArr, sectorsNo):
     for i in range(sectorsNo):
         sprinklersNo = int(input(f"Podaj liczbę zraszaczy w sektorze {i + 1}: "))
 
         for j in range(sprinklersNo):
             sprinklersArr.append([])
-            sprinklersArr[i].append(sprinklerId)
-            sprinklerId += 1
+            sprinklersArr[i].append(input(f"Podaj id zraszacza nr.{j + 1} w sektorze {i + 1}: "))
 
 
 def prepare_humidity_data(humidityArr, sectorsNo):
@@ -23,13 +26,13 @@ def prepare_humidity_data(humidityArr, sectorsNo):
         humidityArr.append(humidity)
 
 
-def assemble_config(sectorsNo, humidityArr, sprinklersArr):
+def assemble_config(sectorsNo, humidityArr, sensorsArr, sprinklersArr):
     data = {"sectors": []}
 
     for i in range(1, sectorsNo + 1):
         sectorObject = {
             "id": i,
-            "sensor_id": i,
+            "sensor_id": sensorsArr[i - 1],
             "desired_humidity": humidityArr[i - 1],
             "sprinklers": sprinklersArr[i - 1]
         }
@@ -55,15 +58,17 @@ if __name__ == '__main__':
     mqttc_server = input("\nPodaj adres brokera MQTT: ")
     mqttc.connect(mqttc_server, 1883, 60)
 
+    sensors = []
     sprinklers = []
     desiredHumidity = []
 
     numberOfSectors = int(input("\nPodaj liczbę sektorów do nawadniania: "))
 
+    prepare_sensors(sensors, numberOfSectors)
     prepare_sprinklers(sprinklers, numberOfSectors)
     prepare_humidity_data(desiredHumidity, numberOfSectors)
 
-    data = assemble_config(numberOfSectors, desiredHumidity, sprinklers)
+    data = assemble_config(numberOfSectors, desiredHumidity, sensors, sprinklers)
     handle_weather_forecast(data)
 
     print(json.dumps(data))
